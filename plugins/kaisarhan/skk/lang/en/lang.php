4942d5e91e89371a19fa648f5ff6d13a49ba1036
Kaisarhan Kuatovich <Kaisarhan@mail.ru>
2023-05-25 00:10:59 +0600

    first commit

---
+++ plugins/kaisarhan/skk/lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'SKK',
+        'description' => 'My First Plugin'
+    ]
+];
---
+++ themes/MED/assets/js/slideDocuments.js
@@ -0,0 +1,7 @@
+const container = document.getElementById("myCarousel");
+const options = { 
+  Dots: false,
+  Navigation: false,
+ };
+
+new Carousel(container, options, { Thumbs });
